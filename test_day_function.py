057f09dddd847e87a04ffe434bbd8ea8c0d912c0
kangying7 <81382017+kangying7@users.noreply.github.com>
2022-04-06 21:52:59 +0800

    Created function to find date of next weekday.

---
+++ test_day_function.py
@@ -0,0 +1,29 @@
+from calendar import FRIDAY, MONDAY, SATURDAY, THURSDAY, TUESDAY, WEDNESDAY, SUNDAY
+import datetime
+
+onDay = lambda date, day: date + datetime.timedelta(days=(day-date.weekday())%7)
+
+def dayOnNextWeek(target_day: datetime.datetime.day):
+    diff_in_days_to_next_monday = datetime.timedelta(days=(MONDAY-datetime.datetime.today().weekday())%7)
+    # print("Difference in days are", diff_in_days)
+    next_monday_date = today + diff_in_days_to_next_monday
+
+    # print("Target day is", target_day)
+    next_week_date = next_monday_date + datetime.timedelta(days=target_day)
+    return next_week_date.strftime("%d/%b/%Y")
+
+# print(onDay(datetime, ))
+
+today = datetime.datetime.today()
+print("Today is", today.date())
+
+print("Next Monday is", dayOnNextWeek(MONDAY))
+print("Next Tuesday is", dayOnNextWeek(TUESDAY))
+print("Next Wednesdaay is", dayOnNextWeek(WEDNESDAY))
+print("Next Thursday is", dayOnNextWeek(THURSDAY))
+print("Next Friday is", dayOnNextWeek(FRIDAY))
+print("Next Saturday is", dayOnNextWeek(SATURDAY))
+print("Next Sunday is", dayOnNextWeek(SUNDAY))
+
+# six_april = datetime.datetime.strptime("09/Apr/2022", "%d/%b/%Y")
+# print(f"six_april is {six_april}")
