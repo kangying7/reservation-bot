d2552ace0bed560cdad5bf82e38167535a90abad
kangying7 <81382017+kangying7@users.noreply.github.com>
2022-06-06 18:06:58 +0800

    Add config file into session folder.

--- async_subprocess.py
+++ async_subprocess.py
@@ -1,6 +1,7 @@
 import asyncio
 from calendar import FRIDAY, MONDAY, SUNDAY, THURSDAY, TUESDAY, WEDNESDAY, day_name, day_abbr
 from configparser import ConfigParser
+import shutil
 from lib.custom_logger import CustomLogger
 from pathlib import Path
 import datetime
@@ -21,28 +22,6 @@ async def run(cmd, logger: CustomLogger):
         logger.add_to_log(f'[stderr]\n{stderr.decode()}')
 
 async def main():
-    # logger = CustomLogger(f"{day_name[raw_day_of_the_week]}.log")
-    config = ConfigParser()
-    config.read("etc/config.txt")
-    single_run = config['single_run']
-    time = single_run.get('before_time_slot')
-    days_to_book = single_run.get('days_to_book')
-    session = single_run.get('session')
-    booking = single_run.getboolean('booking')
-    
-    print("Before time slot - ", time)
-    print("Days to book are - ", days_to_book)
-    
-    # Guard for days to book
-    days_to_book_list = []
-    try:
-        for day in days_to_book.split(","):
-            parsed_day = int(day.strip())
-            days_to_book_list.append(parsed_day)
-    except Exception as e:
-        print(f"Error in config file: days_to_book parameter should be between 1-7 (Monday to Sunday), separated by comma. Error is: {e}")
-        return
-        
     # Create output folder if it does not exist
     log_output_path = Path.cwd() / 'output'
     Path(log_output_path).mkdir(parents=True, exist_ok=True)
@@ -50,6 +29,30 @@ async def main():
     # Create log folder with timestamp
     session_log_path = log_output_path / f'session_{datetime.datetime.now().strftime("%b_%d_%H%M_%S_%f")}'
     Path(session_log_path).mkdir()
+
+    # Copy over source config file to current session directory
+    config_source_file = Path.cwd() / "etc" / "config.txt"
+    config_session_file = shutil.copy(config_source_file, session_log_path)
+
+    config = ConfigParser()
+    config.read(config_session_file)
+    single_run = config['single_run']
+    time = single_run.get('before_time_slot')
+    days_to_book = single_run.get('days_to_book')
+    session = single_run.get('session')
+    booking = single_run.getboolean('booking')
+        
+    # Guard for days to book
+    days_to_book_list = []
+    try:
+        for day in days_to_book.split(","):
+            parsed_day = int(day.strip())
+            days_to_book_list.append(parsed_day)
+    except Exception as e:
+        print(f"Error in config file: days_to_book parameter should be between 0-6 (Monday to Sunday), separated by comma. Error is: {e}")
+        return
+        
+    # Create session_details file for logging purposes
     session_logger = CustomLogger(session_log_path, "session_details.log")
     start_time = datetime.datetime.now()
    
